cc5abfc58e809fba8744c71009f4f78cf34c8daa
Harsha <harshatg2004@gmail.com>
2025-08-10 12:10:59 +0530

    Commited

---
+++ script.js
@@ -0,0 +1,25 @@
+var btc = document.getElementById("bitcoin");
+var eth = document.getElementById("ethereum");
+var doge = document.getElementById("dogecoin");
+
+var settings = {
+  async: true,
+  scrossDomain: true,
+  url: "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
+  method: "GET",
+  headers: {},
+};
+$.ajax(settings).done(function (response) {
+  btc.innerHTML = response.bitcoin.usd;
+  eth.innerHTML = response.ethereum.usd;
+  doge.innerHTML = response.dogecoin.usd;
+});
+
+// const URL =
+//   "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd";
+
+// async function crypto() {
+//   const response = await fetch(URL);
+//   const data = await response.json();
+//   console.log(data);
+// }
